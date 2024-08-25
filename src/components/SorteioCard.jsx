f3c820b0369a23c2d83c22e56f190aea41364de2
alberto <albertoronny237@gmail.com>
2024-08-26 00:15:39 +0100

    fix

--- src/components/SorteioCard.jsx
+++ src/components/SorteioCard.jsx
@@ -5,18 +5,16 @@ function SorteioCard() {
     <div className="max-w-sm rounded-t-2xl bg-white overflow-hidden shadow-lg">
       <img className="w-full" src="https://www.motorrad-bilder.at/thumbs/995x640/slideshows/291/011877/rizoma-yamaha-mt-07-01.jpg" alt="Yamaha MT-07" />
       <div className="px-6 py-4">
-        <div className="font-bold text-gray-700 text-xl mb-2">Sorteio Aniversário Toledo New Car 1 Ano</div>
+        <div className="font-bold text-gray-700 text-center text-2xl mb-2">Sorteio Aniversário Toledo New Car 1 Ano</div>
         <p className="text-gray-700 text-base">
           Essa é sua chance! Essa é sua chance! Essa é sua chance!
           {/* ... repetir a frase ... */}
         </p>
       </div>
-      <div className="px-6 pb-2">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#sorteio</span>   
-
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
-          Bilhetes por apenas R$4,99
-        </span>
+      <div className="px-6 pb-2 items-center flex justify-center">
+        <p className="inline-block bg-gray-600 rounded-2xl px-3 py-2 text-sm text-center font-semibold ">
+          Bilhetes por apenas <br /> <span className='font-bold text-xl'>R$4,99</span>
+        </p>
       </div>
     </div>
   );
