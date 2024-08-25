64764afe0c5f43778d021214d5d3f07fb0893627
alberto <albertoronny237@gmail.com>
2024-08-26 00:33:49 +0100

    fix

--- src/components/SorteioCard.jsx
+++ src/components/SorteioCard.jsx
@@ -5,7 +5,7 @@ function SorteioCard() {
     <div className="max-w-sm rounded-t-2xl rounded-b-lg bg-white overflow-hidden shadow-lg">
       <img className="w-full" src="https://www.motorrad-bilder.at/thumbs/995x640/slideshows/291/011877/rizoma-yamaha-mt-07-01.jpg" alt="Yamaha MT-07" />
       <div className="px-6 py-4">
-        <div className="font-bold text-gray-700 text-center text-2xl mb-2">Sorteio Aniversário Toledo New Car 1 Ano</div>
+        <div className="font-bold text-gray-700 text-center text-xl mb-2">🌟 Entre na Rifa e Ganhe! 🌟</div>
         <p className="text-gray-700 text-base text-center">
         Adquira uma cota e entre no sorteio para ganhar prêmios fantásticos! Cada cota também contribui para boas causas. <br /> <span className='font-semibold text-red-500'>Não perca essa chance!</span>
         </p>
